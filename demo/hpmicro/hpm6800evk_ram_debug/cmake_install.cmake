a0b19147817d0e30e1962b69d67aa1c6128367ff
flose <seeyounextsun@outlook.com>
2025-12-09 12:43:03 +0800

    fix: 重新生成ses工程

--- demo/hpmicro/hpm6800evk_ram_debug/cmake_install.cmake
+++ demo/hpmicro/hpm6800evk_ram_debug/cmake_install.cmake
@@ -40,7 +40,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("C:/Env/cherryECAT/CherryECAT/demo/hpmicro/hpm6800evk_ram_debug/build_tmp/cmake_install.cmake")
-  include("C:/Env/cherryECAT/CherryECAT/demo/hpmicro/hpm6800evk_ram_debug/cherryec/cmake_install.cmake")
+  include("C:/Env/cherryECAT/CherryECAT/demo/hpmicro/hpm6800evk_ram_debug/cherryecat/cmake_install.cmake")
 
 endif()
 
